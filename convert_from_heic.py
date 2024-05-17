9771d49fe17688394404eaa6df0456fff10ada7b
Julia <54853811+sizovaj95@users.noreply.github.com>
2024-05-17 16:29:46 +0100

    script to convert from .heic

---
+++ convert_from_heic.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+
+import PIL
+from PIL import Image
+import re
+from pillow_heif import register_heif_opener
+
+
+path = Path(__file__).parent.resolve() / "test_files"
+
+register_heif_opener()
+
+
+def convert_from_heic(file, remove_heic: bool = False):
+    new_name = re.sub(r"\.[Hh][Ee][Ii][Cc]$", ".jpg", file.name)
+    try:
+        heic_im = Image.open(file.path)
+    except PIL.UnidentifiedImageError as e:
+        print(e)
+        return
+    try:
+        heic_im.convert("RGB").save(path / new_name)
+    except Exception as e:
+        print(e)
+        return
+    if remove_heic:
+        try:
+            os.remove(file.path)
+        except Exception as e:
+            print(e)
+
+
+def main():
+    if not os.path.exists(path):
+        print(f"Folder {path} does not exist!")
+    files = list(os.scandir(path))
+    for file in files:
+        if re.search(r"\.heic$", file.name, re.I):
+            convert_from_heic(file)
+
+
+if __name__ == "__main__":
+    main()
